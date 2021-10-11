3931009ec2174a58043e5c2995fa0b58c596c6d4
nicolas1729 <ns.chevrier+github@gmail.com>
2021-10-11 17:24:22 +0200

    maj test

--- tests/demo.js
+++ tests/demo.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-    'Datepicker' : function (browser) {
+  
+    /*'Datepicker' : function (browser) {
         let page = browser.page.datepicker()
         page.navigate()
           .click('@dateInput')
@@ -32,6 +33,21 @@ module.exports = {
         .click(selectVille)
         .screenshot()
         .end()
-    }
+    }*/
   
   }
+
+  describe('Demo test Ecosia.org', function() {
+    test('search for nightwatch', function(browser) {
+      browser
+        .url('https://www.ecosia.org/')
+        .waitForElementVisible('body')
+        .assert.titleContains('Ecosia')
+        .assert.visible('input[type=search]')
+        .sendKeys('input[type=search]', 'nightwatch')
+        .assert.visible('button[type=submit]')
+        .click('button[type=submit]')
+        .assert.containsText('.mainline-results', 'Nightwatch.js')
+        .end();
+    })
+  });
